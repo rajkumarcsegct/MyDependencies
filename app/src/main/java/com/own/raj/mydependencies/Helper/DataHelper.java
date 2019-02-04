52c6ead83dd50c3199d360ad0eeafa89f60757c9
rajkumar.s <rajkumar.s@ivymobility.com>
2019-02-04 21:22:23 +0530

    just initializing directly

---
+++ app/src/main/java/com/own/raj/mydependencies/Helper/DataHelper.java
@@ -0,0 +1,13 @@
+package com.own.raj.mydependencies.Helper;
+
+public class DataHelper {
+
+    public void DataHelper(){
+
+    }
+
+    public String getDBName(){
+
+        return "v19";
+    }
+}
--- app/src/main/java/com/own/raj/mydependencies/MyActivity.java
+++ app/src/main/java/com/own/raj/mydependencies/MyActivity.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.SupportActivity;
 
+import com.own.raj.mydependencies.Helper.DataHelper;
+
 public class MyActivity extends SupportActivity {
 
+    DataHelper dataHelper;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_myactivity);
+
+        dataHelper=new DataHelper();
+        dataHelper.getDBName();
     }
 }
