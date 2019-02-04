4bdcfb936525edab0c6571fdeeaa390d121d0696
rajkumar.s <rajkumar.s@ivymobility.com>
2019-02-04 21:36:35 +0530

    initial commit

--- app/src/main/java/com/own/raj/mydependencies/MainActivity.java
+++ app/src/main/java/com/own/raj/mydependencies/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_myactivity);
 
+
         AppComponent appComponent=DaggerAppComponent.builder().appModule(new AppModule()).build();
         appComponent.Inject(this);
 
